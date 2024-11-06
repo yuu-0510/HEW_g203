2b88eeca80dc88ca6facfc27a60eeef64641c534
安永有佑 <nhs30477@nhs.hal.ac.jp>
2024-11-06 23:34:24 +0900

    2024/11/06-23:34-Mapクラス作成

--- DX22_Project/Map.h
+++ DX22_Project/Map.h
@@ -1,5 +1,16 @@
 #pragma once
-class CMap
+#include "ObjectBase.h"
+#include "Block.h"
+
+#define MAP_COLUMN		(100)
+#define MAP_ROW			(30)
+
+class CMap : public CObjectBase
 {
+public:
+	CMap();
+	~CMap();
+private:
+	CBlock* m_Map[MAP_ROW][MAP_COLUMN];
 };
 
