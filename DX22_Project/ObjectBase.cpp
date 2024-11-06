b9130ce551fe09c01f17ef97456239f0c76a2482
安永有佑 <nhs30477@nhs.hal.ac.jp>
2024-11-06 23:22:29 +0900

    2024/11/06-23:20-ObjectBase

--- DX22_Project/ObjectBase.cpp
+++ DX22_Project/ObjectBase.cpp
@@ -1,2 +1,26 @@
 
 #include "ObjectBase.h"
+
+CObjectBase::CObjectBase()
+{
+}
+
+CObjectBase::~CObjectBase()
+{
+}
+
+void CObjectBase::Init()
+{
+}
+
+void CObjectBase::Update()
+{
+}
+
+void CObjectBase::Draw()
+{
+}
+
+void CObjectBase::Uninit()
+{
+}
